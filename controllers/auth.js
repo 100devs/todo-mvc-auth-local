93c10450d841f59e242eea0b84e5248e1722329b
Austin_Curran <a.curran1192@gmail.com>
2022-08-13 17:16:33 -0600

    added .env, updated auth.js controller so logout is async

--- controllers/auth.js
+++ controllers/auth.js
@@ -36,14 +36,13 @@ const User = require('../models/User')
     })(req, res, next)
   }
   
+  //updated to be asyncronous
   exports.logout = (req, res) => {
-    req.logout()
-    req.session.destroy((err) => {
-      if (err) console.log('Error : Failed to destroy the session during logout.', err)
-      req.user = null
-      res.redirect('/')
-    })
-  }
+    req.logout(function(err) {
+      if (err) { return next(err); }
+      res.redirect('/');
+    });
+  };
   
   exports.getSignup = (req, res) => {
     if (req.user) {
