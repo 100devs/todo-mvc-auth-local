d65c1c62d381d3ef62df51fd54a980bc3290550d
Leon Noel <leon.a.noel+github@gmail.com>
2022-08-30 13:06:19 -0700

    fixed passport auth error and added env file

--- controllers/auth.js
+++ controllers/auth.js
@@ -37,7 +37,9 @@ const User = require('../models/User')
   }
   
   exports.logout = (req, res) => {
-    req.logout()
+    req.logout(() => {
+      console.log('User has logged out.')
+    })
     req.session.destroy((err) => {
       if (err) console.log('Error : Failed to destroy the session during logout.', err)
       req.user = null
