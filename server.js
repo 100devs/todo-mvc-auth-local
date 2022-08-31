93c79b16b9179cf2868fddece6244df081efefaf
EricGallegos <46502883+EricGallegos@users.noreply.github.com>
2022-08-30 20:03:17 -0600

    typo fix

--- server.js
+++ server.js
@@ -22,6 +22,7 @@ app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(logger('dev'))
+
 // Sessions
 app.use(
     session({
@@ -31,16 +32,16 @@ app.use(
       store: new MongoStore({ mongooseConnection: mongoose.connection }),
     })
   )
-  
+
 // Passport middleware
 app.use(passport.initialize())
 app.use(passport.session())
 
 app.use(flash())
-  
+
 app.use('/', mainRoutes)
 app.use('/todos', todoRoutes)
- 
+
 app.listen(process.env.PORT, ()=>{
     console.log('Server is running, you better catch it!')
-})    
+})
