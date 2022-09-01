2c3710f26271916418a5198023d3bdf1322018ac
romanstetsyk <25715951+romanstetsyk@users.noreply.github.com>
2022-09-01 17:48:54 +0100

    subrtact expenses when creating new budget

--- controllers/budget.js
+++ controllers/budget.js
@@ -23,8 +23,21 @@ const getBudget = async (req, res, next) => {
 
 const createBudget = async (req, res, next) => {
   try {
+    // Find expenses in the period between startDate and endDate
+    const expenses = await Expense.find({
+      $and: [
+        { user: req.user.id },
+        { date: { $gte: req.body.startDate } },
+        { date: { $lte: req.body.endDate } },
+      ],
+    });
+    // Total of the expenses
+    const totalExpenses = expenses.reduce((a, e) => a + e.amount, 0);
+    console.log(`total: ${totalExpenses}`);
+
+    // Create a new budget
     await Budget.create({
-      amount: Number(req.body.amount) * 100,
+      amount: Number(req.body.amount) * 100 - totalExpenses, // Subtract the expenses in this period
       currency: req.body.currency,
       startDate: req.body.startDate,
       endDate: req.body.endDate,
