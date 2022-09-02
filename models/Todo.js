2b3647395c3753f35070d58973e153c38c567bc0
Petya Angelova <bedfordpetya@gmail.com>
2022-09-02 23:43:32 +0300

    dateStamp added

--- models/Todo.js
+++ models/Todo.js
@@ -1,18 +1,21 @@
 const mongoose = require('mongoose')
 
-const TodoSchema = new mongoose.Schema({
-  todo: {
-    type: String,
-    required: true,
+const TodoSchema = new mongoose.Schema(
+  {
+    todo: {
+      type: String,
+      required: true,
+    },
+    completed: {
+      type: Boolean,
+      required: true,
+    },
+    userId: {
+      type: String,
+      required: true
+    },
   },
-  completed: {
-    type: Boolean,
-    required: true,
-  },
-  userId: {
-    type: String,
-    required: true
-  }
-})
+  { timestamps: true }
+)
 
 module.exports = mongoose.model('Todo', TodoSchema)
