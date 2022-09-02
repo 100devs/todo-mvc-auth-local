174ab685f4a3018109de3b1a5c770fb39db08c0a
Bruce <contact@estimatebodyfat.com>
2022-09-02 12:05:39 -0400

    Setup the MongoDB model, create trip functionality and a dashboard that lets you see the trips you created

---
+++ models/trip.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose')
+
+const TripSchema = new mongoose.Schema({
+
+  userId: {
+    type: String,
+    required: true
+  },
+
+  tripTitle: {
+    type: String,
+    required: true,
+  },
+
+  option1: {
+    type: String,
+    required: true,
+  },
+
+  option2: {
+    type: String,
+    required: true,
+  },
+
+  option3: {
+    type: String,
+    required: true,
+  },
+
+  dateFrom: {
+    type: Date,
+    required: true,    
+  },
+
+  dateTo: {
+    type: Date,
+    required: true,    
+  },
+
+vote: [],
+
+tripMembers: {
+  type: String,
+  required: true,
+},
+
+createdAt: {
+  type: Date,
+  default: Date.now,
+},
+
+})
+
+module.exports = mongoose.model('Trip', TripSchema)
