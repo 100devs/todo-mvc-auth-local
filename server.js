ce8bdca771db1ff43574fd94edac2e6dbd137bb6
Y A S <54225481+specialyas@users.noreply.github.com>
2022-09-02 01:39:08 +0100

    Add comments

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// Add packages  and dependecies to the program
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
@@ -10,6 +11,7 @@ const connectDB = require('./config/database')
 const mainRoutes = require('./routes/main')
 const todoRoutes = require('./routes/todos')
 
+// set app use the env file
 require('dotenv').config({path: './config/.env'})
 
 // Passport config
@@ -17,11 +19,15 @@ require('./config/passport')(passport)
 
 connectDB()
 
+// setting the view to ejs
 app.set('view engine', 'ejs')
+// acess static files
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(logger('dev'))
+
+// keep you logged in until you click logout
 // Sessions
 app.use(
     session({
@@ -36,11 +42,14 @@ app.use(
 app.use(passport.initialize())
 app.use(passport.session())
 
+// display flash messages
 app.use(flash())
   
+// redirect routes to defined routes
 app.use('/', mainRoutes)
 app.use('/todos', todoRoutes)
  
+// set up app to listen for request on given port
 app.listen(process.env.PORT, ()=>{
     console.log('Server is running, you better catch it!')
 })    
