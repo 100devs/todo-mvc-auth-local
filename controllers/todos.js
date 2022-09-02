959f1f4ec39d15b4330865e8b945d16150238876
Geordie King <geordie.king@gmail.com>
2022-09-02 02:50:08 +0100

    sparkles: Todos Deadline date now displays in custom format

--- controllers/todos.js
+++ controllers/todos.js
@@ -6,7 +6,7 @@ module.exports = {
         try{
             const todoItems = await Todo.find({userId:req.user.id})
             const itemsLeft = await Todo.countDocuments({userId:req.user.id,completed: false, })
-            res.render('todos.ejs', {todos: todoItems, left: itemsLeft, user: req.user })
+            res.render('todos.ejs', {todos: todoItems, left: itemsLeft, user: req.user , moment: moment  })
         }catch(err){
             console.log(err)
         }
