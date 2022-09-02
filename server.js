c2ac04126c13151e2d6ea6e49a0f721c592c92be
stacksantos <stacksantos@proton.me>
2022-09-01 20:53:20 -0500

    update css

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const session = require('express-session');
 const MongoStore = require('connect-mongo')(session);
 const flash = require('express-flash');
 const logger = require('morgan');
-// const connectDB = require('./config/database');
+const connectDB = require('./config/database');
 const mainRoutes = require('./routes/main');
 const todoRoutes = require('./routes/todos');
 
@@ -15,7 +15,7 @@ require('dotenv').config({ path: './config/.env' });
 // Passport config
 require('./config/passport')(passport);
 
-// connectDB()
+connectDB()
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
@@ -32,7 +32,6 @@ app.use(
 	})
 );
 
-<<<<<<< HEAD
 // Passport middleware
 app.use(passport.initialize());
 app.use(passport.session());
@@ -45,16 +44,3 @@ app.use('/todos', todoRoutes);
 app.listen(process.env.PORT, () => {
 	console.log('Server is running, you better catch it!');
 });
-=======
-app.use(flash())
-  
-app.use('/', mainRoutes)
-app.use('/todos', todoRoutes)
- 
-app.listen(process.env.PORT, ()=>{
-    console.log('Server is running, you better catch it!')
-})    
-
-
-//comment added by nick
->>>>>>> f447e853fc0a45fa255cb623d98e5ee87d1b803c
