b821aed02caed7f27e54ba8ff879b1626d5159ca
ctrich <ctrich86@yahoo.com>
2022-09-02 13:05:23 -0700

    configured passports google strategy

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,9 @@
 const LocalStrategy = require('passport-local').Strategy
+const GoogleStrategy = require('passport-google-oauth2').Strategy
 const mongoose = require('mongoose')
 const User = require('../models/User')
+require('dotenv').config({path: '/.env'})
+
 
 module.exports = function (passport) {
   passport.use(new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
@@ -21,6 +24,36 @@ module.exports = function (passport) {
       })
     })
   }))
+
+  passport.use(new GoogleStrategy({
+    clientID:     process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: "/auth/google/callback",
+  },
+  async (accessToken, refreshToken, profile, done) => {
+      const newUser = {
+        userName: profile.email.slice(0, profile.email.indexOf('@')),
+        email: profile.email,
+        googleId: profile.id,
+      }
+
+      try{
+        let user = await User.findOne( { email: profile.email });
+
+        if (user) {
+          done(null, user);
+        } else {
+          user = await User.create(newUser);
+          done(null, user);
+        }
+      } catch(err) {
+         console.log(err);
+      }
+    }));
+  
+
+
+  
   
 
   passport.serializeUser((user, done) => {
