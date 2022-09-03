afd26bb8ea732f53baef91135f03a10bb2638677
Tyler Perata <tylerperata@gmail.com>
2022-09-03 12:52:42 -0700

    calendar-class

--- controllers/todos.js
+++ controllers/todos.js
@@ -1,25 +1,45 @@
 const Todo = require('../models/Todo')
+const Task = require("../models/Task")
 
 module.exports = {
     getTodos: async (req,res)=>{
         console.log(req.user)
         try{
-            const todoItems = await Todo.find({userId:req.user.id})
-            const itemsLeft = await Todo.countDocuments({userId:req.user.id,completed: false})
-            res.render('todos.ejs', {todos: todoItems, left: itemsLeft, user: req.user})
+            const taskItems = await Task.find({userId:req.user.id})
+            // const itemsLeft = await Todo.countDocuments({userId:req.user.id,completed: false})
+            res.render('todos.ejs', {todos: taskItems, user: req.user})
+            // res.json(taskItems)
         }catch(err){
             console.log(err)
         }
     },
+    //just added the fields we want
     createTodo: async (req, res)=>{
         try{
-            await Todo.create({todo: req.body.todoItem, completed: false, userId: req.user.id})
+           await Task.create({
+                task: req.body.task,
+                day: req.body.day,
+                month: req.body.month,
+                startTime: req.body.startTime,
+                endTime: req.body.endTime,
+                userId: req.user.id
+            })
             console.log('Todo has been added!')
             res.redirect('/todos')
         }catch(err){
-            console.log(err)
+            res.status(400).json({error: err.message})
         }
     },
+    //original function
+    // createTodo: async (req, res)=>{
+    //     try{
+    //         await Todo.create({todo: req.body.todoItem, completed: false, userId: req.user.id})
+    //         console.log('Todo has been added!')
+    //         res.redirect('/todos')
+    //     }catch(err){
+    //         console.log(err)
+    //     }
+    // },
     markComplete: async (req, res)=>{
         try{
             await Todo.findOneAndUpdate({_id:req.body.todoIdFromJSFile},{
@@ -45,11 +65,22 @@ module.exports = {
     deleteTodo: async (req, res)=>{
         console.log(req.body.todoIdFromJSFile)
         try{
-            await Todo.findOneAndDelete({_id:req.body.todoIdFromJSFile})
+            await Task.findOneAndDelete({_id:req.body.todoIdFromJSFile})
             console.log('Deleted Todo')
             res.json('Deleted It')
         }catch(err){
             console.log(err)
+        }
+    },
+    //get todos based on url params like /todos/oct/10
+    // only get todos with month of oct and day of 10
+    //sends tasks back in json
+    getTodosByDate: async (req,res) => {
+        try {
+            const tasks = await Task.find({userId: req.user.id, month: req.params.month, day: req.params.day })
+            res.json(tasks)
+        } catch (error) {
+            res.status(400).json({error: error.message})
         }
     }
 }    
--- public/js/main.js
+++ public/js/main.js
@@ -2,7 +2,7 @@ const deleteBtn = document.querySelectorAll('.del')
 const todoItem = document.querySelectorAll('span.not')
 const todoComplete = document.querySelectorAll('span.completed')
 const dateSelector = document.querySelectorAll('li.day')
-const monthSelector = document.querySelectorAll('.months li a')
+
 
 
 Array.from(deleteBtn).forEach((el)=>{
@@ -17,48 +17,18 @@ Array.from(todoComplete).forEach((el)=>{
     el.addEventListener('click', markIncomplete)
 })
 
-Array.from(dateSelector).forEach((el)=>{
-    el.addEventListener('click', selectDay)
-})
-
-Array.from(monthSelector).forEach((el)=>{
-    el.addEventListener('click', selectMonth)
-})
-
-async function selectDay(){
-    const days = document.getElementById('days').childNodes
-    for(let i = 1; i<days.length-1; i++){
-      if(days[i].firstChild){      
-      days[i].firstChild.classList.remove('selected')}    
-    }
-    const selectedDay = this.firstChild.getAttribute('data-value')
-    this.firstChild.className = 'selected'
-}
-
-async function selectMonth(){
-    const months = document.getElementById('months').childNodes
-    for(let i = 0; i<months.length; i++){
-        if(months[i].firstChild){
-        months[i].firstChild.classList.remove('selected')
-        }
-    }
-    const selectedMonth = this.getAttribute('title')
-    this.className= 'selected'
-
-}
-
-async function deleteTodo(){
-    const todoId = this.parentNode.dataset.id
+async function deleteTodo(id){
     try{
         const response = await fetch('todos/deleteTodo', {
             method: 'delete',
             headers: {'Content-type': 'application/json'},
             body: JSON.stringify({
-                'todoIdFromJSFile': todoId
+                //passing id to make sure it deletes the task with same id
+                'todoIdFromJSFile': id
             })
         })
         const data = await response.json()
-        console.log(data)
+        // console.log(data)
         location.reload()
     }catch(err){
         console.log(err)
@@ -102,3 +72,188 @@ async function markIncomplete(){
 }
 
 
+class Calendar {
+    constructor() {
+        // Keeps track of day, month, year
+        // Starting date is now
+        const today = new Date(Date.now());
+        this.day = today.getDate(); // JS Date represents days as 1-31
+        this.month = today.getMonth(); // Represents months from 0-11 (Jan-Dec)
+        this.year = today.getFullYear(); // Represents years as four digit years (2022)
+
+        this.daysContainer = document.querySelector("#days");
+        this.days = [];
+
+        // Info Elements
+        this.taskList = document.querySelector(".taskList");
+
+        // Calendar Elements
+
+        // Initialize calendar functionality
+        // Add event listener to year form
+
+        // Add event listener to month list
+        this.monthSelector = document.querySelectorAll('.months li a');
+        this.activate_month_selector();
+
+        // Render calendar
+        this.render_calendar();
+
+        this.daysContainer.querySelector(`a[data-value="${this.day}"]`).classList.add("selected")
+    }  
+
+    render_calendar() {
+        // Clear calendar
+        while(this.daysContainer.firstChild) {
+            this.daysContainer.removeChild(this.daysContainer.firstChild);
+        }
+        this.days = [];
+        // Calc first day of month
+        let firstDay = new Date(this.year, this.month, 1) // Using 1 since we always want first day of month
+        firstDay = firstDay.getDay(); // getDay() returns the day of the week Sun-Sat (0-6)
+
+        // Since our calendar is Mon-Sun, need to subtract one to get correct starting day. Sunday needs to be set to 6, since it is 0 by default and subtracting 1 just makes it -1
+        firstDay = (firstDay-1 < 0) ? 6 : firstDay-1;
+
+        // Calc days in month
+        // https://stackoverflow.com/questions/1184334/get-number-days-in-a-specified-month-using-javascript
+        let daysInMonth = new Date(this.year, this.month+1, 0); // Gets a date that is set to the last day of the current month
+        daysInMonth = daysInMonth.getDate();
+
+        console.log(daysInMonth);
+
+        // Add offset lis to list
+        // Offset lis are lis containing an anchor that is disabled (no pointer events), so that the month starts at the proper day
+        for(let i = 0; i < firstDay; ++i) {
+            const offsetLi = document.createElement("li");
+            const anchor = document.createElement("a");
+            anchor.classList.add("disabled");
+            offsetLi.appendChild(anchor);
+            this.daysContainer.appendChild(offsetLi);
+        }
+
+        // Add days to list
+        for(let i = 1; i <= daysInMonth; ++i) {
+            const dayLi = document.createElement("li");
+            dayLi.classList.add("day");
+
+            const a = document.createElement("a");
+            a.dataset.value = i;
+            a.title = i;
+            a.innerText = i;
+
+            dayLi.appendChild(a);
+
+            this.daysContainer.appendChild(dayLi);
+            this.days.push(a);
+        }
+
+        // Actiate days
+        this.activate_day_selector();
+        this.render_day();
+    }
+
+    activate_month_selector() {
+        Array.from(this.monthSelector).forEach( (el)=> el.addEventListener('click', this.select_month.bind(this)) );
+    }
+
+    select_month(event) {
+        const months = document.getElementById('months').childNodes
+        for(let i = 0; i<months.length; i++){
+            if(months[i].firstChild){
+            months[i].firstChild.classList.remove('selected')
+            }
+        }
+        const selectedMonth = event.target.getAttribute('title')
+        event.target.className= 'selected'
+
+        this.month = convert_month_to_num(selectedMonth);
+
+        // Re-render calendar
+        this.render_calendar();
+    }
+
+    activate_day_selector() {
+        console.log(this.days);
+        this.days.forEach(x => x.addEventListener("click", this.change_day.bind(this)));
+    }
+
+    change_day(event) {
+        const selectedDay = event.target.dataset.value;
+        this.day = selectedDay;
+
+        const days = document.getElementById('days').childNodes
+        for(let i = 1; i<days.length-1; i++){
+            if(days[i].firstChild){      
+            days[i].firstChild.classList.remove('selected')}    
+        }
+        event.target.classList.add("selected");
+
+        // Now, render day info
+        this.render_day();
+    }
+
+    async render_day() {
+        // Change up header
+        const today = new Date(this.year, this.month, this.day);
+        document.querySelector(".todays-day").innerText = get_day(today.getDay());
+
+        document.querySelector(".month-day").innerText = `${get_month(this.month)} ${this.day}`
+
+        // Clear tasks from task list
+        while(this.taskList.firstChild) {
+            this.taskList.removeChild(this.taskList.firstChild);
+        }
+
+        // Get tasks
+        const res = await fetch(`/todos/${this.month}/${this.day}`);
+        const data = await res.json();
+
+        // Append tasks to task list
+        if(data.length == 0) {
+            const item = document.createElement("li");
+            item.innerText = "No tasks to display";
+            this.taskList.appendChild(item);
+        }
+        else
+        {
+            data.forEach( x => {
+                const li = document.createElement("li");
+                li.classList.add("todoItem");
+                li.dataset.id = x._id;
+
+                li.innerHTML = `<span class="filteredTasks">${x.task}</span>
+                                <a href="#" title="Remove note" class="removeNote animate del" onclick="deleteTodo(this.parentNode.dataset.id)">x</a>
+                                <span style="display: block">${x.startTime} - ${x.endTime}</span>`
+
+                this.taskList.appendChild(li);
+            });
+        }
+
+        // Add proper dates to form
+        document.querySelector(".dbDay").value = this.day;
+        document.querySelector(".dbMonth").value = this.month;
+    }
+}
+
+// Class helpers
+function convert_month_to_num(month) {
+    month = month.toLowerCase(); // Convert 3 char month code to all lower case
+    const months = ["jan","feb","mar","apr","may","jun","jul","aug","sep","oct","nov","dec"];
+    return months.indexOf(month); // Return its index in the array (0-11)
+}
+
+function get_day(numericDay) {
+    const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    return days[numericDay];
+}
+
+function get_month(numericMonth) {
+    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    return months[numericMonth];
+}
+
+
+
+// Start calendar
+const cal = new Calendar;
