69dc2657791516c679b4d54ac6049b8c36197ae0
Christian Larson <larsonchristian505@gmail.com>
2022-09-03 00:59:48 -0700

    swiper design v1

--- public/js/main.js
+++ public/js/main.js
@@ -2,6 +2,30 @@ const deleteBtn = document.querySelectorAll('.del')
 const todoItem = document.querySelectorAll('span.not')
 const todoComplete = document.querySelectorAll('span.completed')
 
+
+//do not uncomment these 2 lines below! for some reason this import method breaks swiper :x
+// import Swiper from 'swiper/bundle';
+// import 'swiper/css/bundle';
+
+const swiper = new Swiper('.swiper', {
+      slidesPerView: 3,
+      spaceBetween: 30,
+      loop: true,
+    //   loopFillGroupWithBlank: true,
+
+    navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+    });
+
+
+
+    
 Array.from(deleteBtn).forEach((el)=>{
     el.addEventListener('click', deleteTodo)
 })
