1beefcfa054b7334f1c9a4629787a90d00c9cb08
David <davidhenrydev@gmail.com>
2022-09-03 15:42:34 -0400

    adding gear to DB working

--- controllers/gear.js
+++ controllers/gear.js
@@ -1,15 +1,39 @@
 const Gear = require("../models/Gear"); // Requires the model schema
 
 module.exports = {
+  getGear: async (req, res) => {
+    try {
+      res.render("gear.ejs");
+    } catch (err) {
+      console.error(err);
+    }
+  },
+
   createGear: async (req, res) => {
-    console.log("createGear function started.");
-    res.render("itWorks.ejs");
+    {
+      try{
+          await Gear.create({
+            gearName: req.body.gearName, 
+            gearBrand: req.body.gearBrand, 
+            gearPrice: req.body.gearPrice, 
+            gearCondition: req.body.gearCondition, 
+            gearNote: req.body.gearNote, 
+            completed: false, 
+            userId: req.user.id})
+          
+            console.log(`${req.body.gearName} ${req.body.gearBrand} has been added!`)
+            res.redirect('/gear')
+
+      }catch(err){
+          console.log(err)
+      }
+  }
     // try {
     //   await Gear.create({
     //     gearName: String,
     //     gearBrand: String,
     //     gearPrice: Number,
-    //     gearCondiditon: String,
+    //     gearCondition: String,
     //     gearNote: req.gearNote,
     //     visibility: "public",
     //     userId: req.user.id,
@@ -18,11 +42,4 @@ module.exports = {
     //   console.error(err);
     // }
   },
-  getGear: async (req, res) => {
-    try {
-      res.render("gear.ejs");
-    } catch (err) {
-      console.error(err);
-    }
-  },
-};
+}
