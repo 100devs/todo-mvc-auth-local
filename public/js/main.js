8cc217cac5fc21fd5c2c9ffd3481807ddf2db0b9
j-mataway <j.m.mataway@gmail.com>
2022-09-02 20:17:39 -0400

    day selector and month selector working

--- public/js/main.js
+++ public/js/main.js
@@ -2,6 +2,9 @@ const deleteBtn = document.querySelectorAll('.del')
 const todoItem = document.querySelectorAll('span.not')
 const todoComplete = document.querySelectorAll('span.completed')
 const dateSelector = document.querySelectorAll('li.day')
+const monthSelector = document.querySelectorAll('.months li a')
+
+
 Array.from(deleteBtn).forEach((el)=>{
     el.addEventListener('click', deleteTodo)
 })
@@ -18,9 +21,31 @@ Array.from(dateSelector).forEach((el)=>{
     el.addEventListener('click', selectDay)
 })
 
+Array.from(monthSelector).forEach((el)=>{
+    el.addEventListener('click', selectMonth)
+})
+
 async function selectDay(){
-    console.log(this)
+    const days = document.getElementById('days').childNodes
+    for(let i = 0; i<days.length; i++){
+        if(days[i].firstChild){
+        days[i].firstChild.classList.remove('selected')
+        }
+    }
+    const selectedDay = this.firstChild.getAttribute('data-value')
     this.firstChild.className = 'selected'
+
+}
+async function selectMonth(){
+    const months = document.getElementById('months').childNodes
+    for(let i = 0; i<months.length; i++){
+        if(months[i].firstChild){
+        months[i].firstChild.classList.remove('selected')
+        }
+    }
+    const selectedMonth = this.getAttribute('title')
+    console.log(selectedMonth)
+    this.className= 'selected'
 }
 
 async function deleteTodo(){
