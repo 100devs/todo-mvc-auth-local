991fa984f107fbbe418ef10e53da718057ecdd9d
Laura <moomal.abro@gmail.com>
2022-09-03 16:13:30 +0500

    fixed logout for real

--- controllers/auth.js
+++ controllers/auth.js
@@ -36,10 +36,7 @@ const User = require('../models/User')
     })(req, res, next)
   }
 
-  exports.logout = (req, res) => {
-    req.logout(() => {
-      console.log('User has logged out.')
-    })
+exports.logout = (req, res) => {
     req.session.destroy((err) => {
       if (err) console.log('Error : Failed to destroy the session during logout.', err)
       req.user = null
