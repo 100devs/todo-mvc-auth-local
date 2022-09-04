c35ee7774e3d576962ce65a9d6bfb3212f5c18a7
Curtis Barnard <curtis.barnard.jr@gmail.com>
2022-09-04 11:42:25 -0700

    add get single recipe and create recipe routes

--- routes/recipes.js
+++ routes/recipes.js
@@ -3,6 +3,13 @@ const router = express.Router();
 const recipesController = require('../controllers/recipes');
 const { ensureAuth } = require('../middleware/auth');
 
+// Will get all recipes for a user
 router.get('/', ensureAuth, recipesController.getRecipes);
+
+// Will get a single recipe by id
+router.get('/:id', ensureAuth, recipesController.getOneRecipe);
+
+// Will add a new recipe
+router.post('/', ensureAuth, recipesController.addRecipe);
 
 module.exports = router;
