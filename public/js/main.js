657bfa3074b60a7649852a23d810e970db41f286
Ryan <ryanmahara@gmail.com>
2022-09-03 20:39:00 -0500

    format main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,75 +1,94 @@
-const deleteBtn = document.querySelectorAll('.del')
-const companyItem = document.querySelectorAll('span.not')
-const companyComplete = document.querySelectorAll('span.completed')
+const deleteBtn = document.querySelectorAll('.del');
+const companyItem = document.querySelectorAll('span.not');
+const companyComplete = document.querySelectorAll('span.completed');
+const editBtn = document.querySelectorAll('.edit');
 
-Array.from(deleteBtn).forEach((el)=>{
-    el.addEventListener('click', deleteTodo)
-})
+Array.from(deleteBtn).forEach((el) => {
+  el.addEventListener('click', deleteCompany);
+});
 
-Array.from(companyItem).forEach((el)=>{
-    el.addEventListener('click', markComplete)
-})
+Array.from(editBtn).forEach((el) => {
+  el.addEventListener('click', editCompany);
+});
 
-Array.from(companyComplete).forEach((el)=>{
-    el.addEventListener('click', markIncomplete)
-})
+Array.from(companyItem).forEach((el) => {
+  el.addEventListener('click', markComplete);
+});
 
-async function deleteTodo(){
-    const companyId = this.parentNode.dataset.id
-    try{
-        const response = await fetch('companies/deleteCompany', {
-            method: 'delete',
-            headers: {'Content-type': 'application/json'},
-            body: JSON.stringify({
-                'companyIdFromJSFile': companyId
-            })
-        })
-        const data = await response.json()
-        console.log(data)
-        location.reload()
-    }catch(err){
-        console.log(err)
-    }
+Array.from(companyComplete).forEach((el) => {
+  el.addEventListener('click', markIncomplete);
+});
+
+async function deleteCompany() {
+  const companyId = this.parentNode.dataset.id;
+  try {
+    const response = await fetch('companies/deleteCompany', {
+      method: 'delete',
+      headers: { 'Content-type': 'application/json' },
+      body: JSON.stringify({
+        companyIdFromJSFile: companyId,
+      }),
+    });
+    const data = await response.json();
+    console.log(data);
+    location.reload();
+  } catch (err) {
+    console.log(err);
+  }
 }
 
-async function markComplete(e){
-    // guard clause to not trigger markcomplete on button clicks 
-    if(e.target.parentNode.nodeName === "BUTTON" ) return null
-
-    try{
-        const companyId = this.parentNode.dataset.id
-        const response = await fetch('companies/markComplete', {
-            method: 'put',
-            headers: {'Content-type': 'application/json'},
-            body: JSON.stringify({
-                'companyIdFromJSFile': companyId
-            })
-        })
-        const data = await response.json()
-        console.log(data)
-        location.reload()
-    }catch(err){
-        console.log(err)
-    }
+function editCompany() {
+  const companyId = this.parentNode.dataset.id;
+  window.location = `companies/edit/${companyId}`;
+  //   try {
+  //     const response = await fetch(`companies/edit/${companyId}`);
+  //     console.log(response);
+  //     // const data = await response.json()
+  //     // console.log(data)
+  //     // location.reload()
+  //   } catch (err) {
+  //     console.log(err);
+  //   }
 }
 
-async function markIncomplete(e){
-    // guard clause to not trigger markcomplete on button clicks 
-    if(e.target.parentNode.nodeName === "BUTTON" ) return null
-    
-    const companyId = this.parentNode.dataset.id
-    try{
-        const response = await fetch('companies/markIncomplete', {
-            method: 'put',
-            headers: {'Content-type': 'application/json'},
-            body: JSON.stringify({
-                'companyIdFromJSFile': companyId
-            })
-        })
-        const data = await response.json()
-        console.log(data)
-        location.reload()
-    }catch(err){
-        console.log(err)
-    }
+async function markComplete(e) {
+  // guard clause to not trigger markcomplete on button clicks
+  if (e.target.parentNode.nodeName === 'BUTTON') return null;
+
+  try {
+    const companyId = this.parentNode.dataset.id;
+    const response = await fetch('companies/markComplete', {
+      method: 'put',
+      headers: { 'Content-type': 'application/json' },
+      body: JSON.stringify({
+        companyIdFromJSFile: companyId,
+      }),
+    });
+    const data = await response.json();
+    console.log(data);
+    location.reload();
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+async function markIncomplete(e) {
+  // guard clause to not trigger markcomplete on button clicks
+  if (e.target.parentNode.nodeName === 'BUTTON') return null;
+
+  const companyId = this.parentNode.dataset.id;
+  try {
+    const response = await fetch('companies/markIncomplete', {
+      method: 'put',
+      headers: { 'Content-type': 'application/json' },
+      body: JSON.stringify({
+        companyIdFromJSFile: companyId,
+      }),
+    });
+    const data = await response.json();
+    console.log(data);
+    location.reload();
+  } catch (err) {
+    console.log(err);
+  }
 }
