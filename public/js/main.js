cdbd4421974f2aad4e54facabff679b68138ef1a
“bomboloni” <slarge1+github@gmail.com>
2022-09-03 19:59:39 -0700

    added back delete functionality

--- public/js/main.js
+++ public/js/main.js
@@ -15,23 +15,23 @@ Array.from(todoComplete).forEach((el)=>{
     el.addEventListener('click', markIncomplete)
 })
 
-// async function deleteTodo(){
-//     const todoId = this.parentNode.dataset.id
-//     try{
-//         const response = await fetch('todos/deleteTodo', {
-//             method: 'delete',
-//             headers: {'Content-type': 'application/json'},
-//             body: JSON.stringify({
-//                 'todoIdFromJSFile': todoId
-//             })
-//         })
-//         const data = await response.json()
-//         console.log(data)
-//         location.reload()
-//     }catch(err){
-//         console.log(err)
-//     }
-// }
+async function deleteTodo(){
+    const entryId = this.parentNode.dataset.id
+    try{
+        const response = await fetch('entries/deleteEntry', {
+            method: 'delete',
+            headers: {'Content-type': 'application/json'},
+            body: JSON.stringify({
+                'entryIdFromJSFile': entryId
+            })
+        })
+        const data = await response.json()
+        console.log(data)
+        location.reload()
+    }catch(err){
+        console.log(err)
+    }
+}
 
 // async function markComplete(){
 //     const todoId = this.parentNode.dataset.id
