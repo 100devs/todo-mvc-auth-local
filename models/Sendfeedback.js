dc0156357b14a620de9c82576fd22cab6df5bae8
Tyler Alexander <TylerGeorgeAlexander@gmail.com>
2022-09-03 23:31:24 -0700

    created schema for sendfeedback

---
+++ models/Sendfeedback.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+
+const SendfeedbackSchema = new mongoose.Schema({
+    name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true
+  },
+  message: {
+    type: String,
+    required: true
+  }
+})
+
+module.exports = mongoose.model('Sendfeedback', SendfeedbackSchema)
