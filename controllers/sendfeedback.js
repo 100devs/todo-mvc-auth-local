ea7787560ebae86d2976ea35abec788690e7ff53
Tyler Alexander <TylerGeorgeAlexander@gmail.com>
2022-09-04 00:09:29 -0700

    test resposonse with view

--- controllers/sendfeedback.js
+++ controllers/sendfeedback.js
@@ -1,30 +1,32 @@
-// createSendfeedback
-
-const Sendfeedback = require('../models/Sendfeedback')
+const Sendfeedback = require("../models/Sendfeedback");
 
 module.exports = {
-    getSendfeedback: async (req,res)=>{
-        console.log(req)
-        try{
-            // const todoItems = await Sendfeedback.find({userId:req.user.id})
-            // const itemsLeft = await Sendfeedback.countDocuments({userId:req.user.id,completed: false})
-            // res.render('sendfeedback.ejs', {todos: todoItems, left: itemsLeft, user: req.user})
-            res.render('sendfeedback.ejs', {test:"Hello"})
-            // res.send({Test: "Hello"})
-        }catch(err){
-            console.log(err)
-        }
-    },
+  getSendfeedback: async (req, res) => {
+    console.log(req);
+    try {
+      // const todoItems = await Sendfeedback.find({userId:req.user.id})
+      // const sendfeedback.find({})
+      // const itemsLeft = await Sendfeedback.countDocuments({userId:req.user.id,completed: false})
+      // res.render('sendfeedback.ejs', {todos: todoItems, left: itemsLeft, user: req.user})
+      res.render("sendfeedback.ejs", { test: "Hello" });
+      // res.send({Test: "Hello"})
+    } catch (err) {
+      console.log(err);
+    }
+  },
 
-    createSendfeedback: async (req, res)=>{
-        try{
-            await Sendfeedback.create({name: req.body.name, email: req.body.email, phone: req.body.phone, message: req.body.message})
-            console.log('Sendfeedback has been added!')
-            res.redirect('/sendfeedback')
-        }catch(err){
-            console.log(err)
-        }
-    },
-
- 
-}    
+  createSendfeedback: async (req, res) => {
+    try {
+      await Sendfeedback.create({
+        name: req.body.name,
+        email: req.body.email,
+        phone: req.body.phone,
+        message: req.body.message,
+      });
+      console.log("Sendfeedback has been added!");
+      res.redirect("/sendfeedback");
+    } catch (err) {
+      console.log(err);
+    }
+  },
+};
