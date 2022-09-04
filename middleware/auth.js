7c5ae3b81d0888ff8e97af039c572bcdf93de1cc
Angra974 <thierryparlier97440@gmail.com>
2022-09-04 15:12:32 +0400

    added partial for input and input with Icons. Updated a bit login/signup forms with.

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,7 +1,6 @@
 module.exports = {
-    ensureAuth: function (req, res, next) {
-    res.locals.loggedIn = req.isAuthenticated();
-      if (res.locals.loggedIn) {
+     ensureAuth: function (req, res, next) {
+      if (req.isAuthenticated()) {
         return next()
       }
       res.redirect('/login')
