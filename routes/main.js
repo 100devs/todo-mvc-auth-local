c19c9509a805a2df21ae2b7cb57c5aeaa9d3cbb7
Tyler Alexander <TylerGeorgeAlexander@gmail.com>
2022-09-04 17:19:48 -0700

    removed sendfeedback to be modular

--- routes/main.js
+++ routes/main.js
@@ -1,18 +1,14 @@
-const express = require('express')
-const router = express.Router()
-const authController = require('../controllers/auth') 
-const homeController = require('../controllers/home')
-const sendfeedbackController = require('../controllers/sendfeedback')
-const { ensureAuth, ensureGuest } = require('../middleware/auth')
+const express = require("express");
+const router = express.Router();
+const authController = require("../controllers/auth");
+const homeController = require("../controllers/home");
+const { ensureAuth, ensureGuest } = require("../middleware/auth");
 
-router.get('/', homeController.getIndex)
-router.get('/login', authController.getLogin)
-router.post('/login', authController.postLogin)
-router.get('/logout', authController.logout)
-router.get('/signup', authController.getSignup)
-router.post('/signup', authController.postSignup)
+router.get("/", homeController.getIndex);
+router.get("/login", authController.getLogin);
+router.post("/login", authController.postLogin);
+router.get("/logout", authController.logout);
+router.get("/signup", authController.getSignup);
+router.post("/signup", authController.postSignup);
 
-router.get('/sendfeedback', sendfeedbackController.getSendfeedback)
-router.post('/sendfeedback', sendfeedbackController.createSendfeedback)
-
-module.exports = router
+module.exports = router;
