cef46e06f97c5dd4f1cad48d6b1bb47d54c60fdc
Tyler Alexander <TylerGeorgeAlexander@gmail.com>
2022-09-04 17:18:50 -0700

    added userId to schema

--- models/Sendfeedback.js
+++ models/Sendfeedback.js
@@ -1,15 +1,15 @@
 const mongoose = require("mongoose");
 
 const SendfeedbackSchema = new mongoose.Schema({
+  userId: {
+    type: String,
+    required: true,
+  },
   name: {
     type: String,
     required: true,
   },
   email: {
-    type: String,
-    required: true,
-  },
-  phone: {
     type: String,
     required: true,
   },
