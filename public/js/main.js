eafcffe6d9f042b53adc237e5a2fffd78ff02b8f
Petya Angelova <bedfordpetya@gmail.com>
2022-09-06 01:06:55 +0300

    added markImportant and markNotImportant functions

--- public/js/main.js
+++ public/js/main.js
@@ -2,6 +2,8 @@ const deleteBtn = document.querySelectorAll('.del')
 const todoItem = document.querySelectorAll('span.not')
 const todoComplete = document.querySelectorAll('span.completed')
 const getTask = document.querySelector('.double')
+const todoImportant = document.querySelectorAll('.fa-star')
+const todoNotImportant = document.querySelectorAll('.fa-star.important')
 console.log(getTask)
 
 Array.from(deleteBtn).forEach((el)=>{
@@ -15,6 +17,15 @@ Array.from(todoItem).forEach((el)=>{
 Array.from(todoComplete).forEach((el)=>{
     el.addEventListener('click', markIncomplete)
 })
+
+Array.from(todoImportant).forEach((el) => {
+    el.addEventListener('click', markImportant)
+})
+
+Array.from(todoNotImportant).forEach((el) => {
+    el.addEventListener('click', markNotImportant)
+})
+
 
 async function deleteTodo(){
     const todoId = this.parentNode.dataset.id
@@ -66,6 +77,42 @@ async function markIncomplete(){
         console.log(data)
         location.reload()
     }catch(err){
+        console.log(err)
+    }
+}
+
+async function markImportant() {
+    const todoId = this.parentNode.dataset.id
+    try {
+        const response = await fetch('todos/markImportant', {
+            method: 'put',
+            headers: { 'Content-type': 'application/json' },
+            body: JSON.stringify({
+                'todoIdFromJSFile': todoId
+            })
+        })
+        const data = await response.json()
+        console.log(data)
+        location.reload()
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+async function markNotImportant() {
+    const todoId = this.parentNode.dataset.id
+    try {
+        const response = await fetch('todos/markNotImportant', {
+            method: 'put',
+            headers: { 'Content-type': 'application/json' },
+            body: JSON.stringify({
+                'todoIdFromJSFile': todoId
+            })
+        })
+        const data = await response.json()
+        console.log(data)
+        location.reload()
+    } catch (err) {
         console.log(err)
     }
 }
