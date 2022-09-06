272c78838be47c768ebfbf3fbc13344f3f8f16dd
MustafaAliDS <dsalimustafa@gmail.com>
2022-09-06 16:59:26 -0700

    chore: changed mongoose line to match updates

--- config/database.js
+++ config/database.js
@@ -1,19 +1,14 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.DB_STRING, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useFindAndModify: false,
-      useCreateIndex: true
-    })
+    const conn = await mongoose.connect(process.env.DB_STRING, {});
 
-    console.log(`MongoDB Connected: ${conn.connection.host}`)
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (err) {
-    console.error(err)
-    process.exit(1)
+    console.error(err);
+    process.exit(1);
   }
-}
+};
 
-module.exports = connectDB
+module.exports = connectDB;
