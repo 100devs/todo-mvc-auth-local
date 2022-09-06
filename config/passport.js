520c2c25b92d043a2bbc978bef8d420ba3fec808
Dorian C Brown <brown.3794@gmail.com>
2022-09-06 17:25:44 -0400

    fix: :fire: Remove magic-link code from passportjs
    remove magic link strategy from passport js

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,5 @@
 const LocalStrategy = require("passport-local").Strategy;
 const mongoose = require("mongoose");
-const MagicLoginStrategy = require("passport-magic-login");
 const User = require("../models/User");
 
 module.exports = function (passport) {
@@ -38,43 +37,4 @@ module.exports = function (passport) {
   passport.deserializeUser((id, done) => {
     User.findById(id, (err, user) => done(err, user));
   });
-};
-
-const magicLogin = new MagicLoginStrategy({
-  secret: process.env.MAGIC_LINK_SECRET,
-
-  // The authentication callback URL
-  callbackUrl: "/auth/magiclogin/callback",
-
-  // Called with th e generated magic link so you can send it to the user
-  // "destination" is what you POST-ed from the client
-  // "href" is your confirmUrl with the confirmation token,
-  // for example "/auth/magiclogin/confirm?token=<longtoken>"
-  sendMagicLink: async (destination, href) => {
-    await sendEmail({
-      to: destination,
-      body: `Click this link to finish logging in: http://localhost:2121/${href}`,
-    });
-  },
-
-  // Once the user clicks on the magic link and verifies their login attempt,
-  // you have to match their email to a user record in the database.
-  // If it doesn't exist yet they are trying to sign up so you have to create a new one.
-  // "payload" contains { "destination": "email" }
-  // In standard passport fashion, call callback with the error as the first argument (if there was one)
-  // and the user data as the second argument!
-  verify: (payload, callback) => {
-    // Get or create a user with the provided email from the database
-    getOrCreateUserWithEmail(payload.destination)
-      .then((user) => {
-        callback(null, user);
-      })
-      .catch((err) => {
-        callback(err);
-      });
-  },
-});
-
-module.exports = function (passportMagicLink) {
-  passport.use(magicLogin);
 };
