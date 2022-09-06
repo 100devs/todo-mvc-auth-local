655276d9ec45c3899c03fb7ca4922737332ecc7f
Andy-git985 <andy.yu617@gmail.com>
2022-09-05 20:46:17 -0400

    moved functions inside try of delete and add

--- public/js/main.js
+++ public/js/main.js
@@ -40,11 +40,11 @@ async function deleteTodo() {
     });
     const data = await response.json();
     // location.reload();
+    todoElem.remove();
+    confettiShower();
   } catch (err) {
     console.log(err);
   }
-  todoElem.remove();
-  confettiShower();
 }
 
 async function addShame() {
@@ -67,13 +67,11 @@ async function addShame() {
     // shame = newShame
     // const placeToShame = document.querySelector('#shaming');
     // placeToShame.textContent = 'test';
+    shameMsg.textContent = newShame;
+    isItTime(newShame);
   } catch (err) {
     console.log(err);
   }
-  // console.log(newShame);
-  // return newShame;
-  shameMsg.textContent = newShame;
-  isItTime(newShame);
 }
 
 // async function markComplete() {
@@ -138,19 +136,19 @@ function isItTime(toDontAmount = null) {
 
 // for starting the confetti
 
-const start = () => {
+function start() {
   setTimeout(function () {
     confetti.start();
   }, 0); // 1000 is time that after 1 second start the confetti ( 1000 = 1 sec)
-};
+}
 
 //  for stopping the confetti
 
-const stop = () => {
+function stop() {
   setTimeout(function () {
     confetti.stop();
   }, 5000); // 5000 is time that after 5 second stop the confetti ( 5000 = 5 sec)
-};
+}
 // after this here we are calling both the function so it works
 // start();
 // stop();
