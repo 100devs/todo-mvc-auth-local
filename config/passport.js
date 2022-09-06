9d23a5e0664a824cf339c5ecb9d2742192bd3f2e
Dorian C Brown <brown.3794@gmail.com>
2022-09-06 09:46:57 -0400

    feat: :package: add magic login package

--- config/passport.js
+++ config/passport.js
@@ -1,33 +1,80 @@
-const LocalStrategy = require('passport-local').Strategy
-const mongoose = require('mongoose')
-const User = require('../models/User')
+const LocalStrategy = require("passport-local").Strategy;
+const mongoose = require("mongoose");
+const MagicLoginStrategy = require("passport-magic-login");
+const User = require("../models/User");
 
 module.exports = function (passport) {
-  passport.use(new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
-    User.findOne({ email: email.toLowerCase() }, (err, user) => {
-      if (err) { return done(err) }
-      if (!user) {
-        return done(null, false, { msg: `Email ${email} not found.` })
-      }
-      if (!user.password) {
-        return done(null, false, { msg: 'Your account was registered using a sign-in provider. To enable password login, sign in using a provider, and then set a password under your user profile.' })
-      }
-      user.comparePassword(password, (err, isMatch) => {
-        if (err) { return done(err) }
-        if (isMatch) {
-          return done(null, user)
+  passport.use(
+    new LocalStrategy({ usernameField: "email" }, (email, password, done) => {
+      User.findOne({ email: email.toLowerCase() }, (err, user) => {
+        if (err) {
+          return done(err);
         }
-        return done(null, false, { msg: 'Invalid email or password.' })
-      })
+        if (!user) {
+          return done(null, false, { msg: `Email ${email} not found.` });
+        }
+        if (!user.password) {
+          return done(null, false, {
+            msg: "Your account was registered using a sign-in provider. To enable password login, sign in using a provider, and then set a password under your user profile.",
+          });
+        }
+        user.comparePassword(password, (err, isMatch) => {
+          if (err) {
+            return done(err);
+          }
+          if (isMatch) {
+            return done(null, user);
+          }
+          return done(null, false, { msg: "Invalid email or password." });
+        });
+      });
     })
-  }))
-  
+  );
 
   passport.serializeUser((user, done) => {
-    done(null, user.id)
-  })
+    done(null, user.id);
+  });
 
   passport.deserializeUser((id, done) => {
-    User.findById(id, (err, user) => done(err, user))
-  })
-}
+    User.findById(id, (err, user) => done(err, user));
+  });
+};
+
+const magicLogin = new MagicLoginStrategy({
+  secret: process.env.MAGIC_LINK_SECRET,
+
+  // The authentication callback URL
+  callbackUrl: "/auth/magiclogin/callback",
+
+  // Called with th e generated magic link so you can send it to the user
+  // "destination" is what you POST-ed from the client
+  // "href" is your confirmUrl with the confirmation token,
+  // for example "/auth/magiclogin/confirm?token=<longtoken>"
+  sendMagicLink: async (destination, href) => {
+    await sendEmail({
+      to: destination,
+      body: `Click this link to finish logging in: http://localhost:2121/${href}`,
+    });
+  },
+
+  // Once the user clicks on the magic link and verifies their login attempt,
+  // you have to match their email to a user record in the database.
+  // If it doesn't exist yet they are trying to sign up so you have to create a new one.
+  // "payload" contains { "destination": "email" }
+  // In standard passport fashion, call callback with the error as the first argument (if there was one)
+  // and the user data as the second argument!
+  verify: (payload, callback) => {
+    // Get or create a user with the provided email from the database
+    getOrCreateUserWithEmail(payload.destination)
+      .then((user) => {
+        callback(null, user);
+      })
+      .catch((err) => {
+        callback(err);
+      });
+  },
+});
+
+module.exports = function (passportMagicLink) {
+  passport.use(magicLogin);
+};
