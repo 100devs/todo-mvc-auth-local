f3c0ef3fbc3912c006678182185e4686b21690ee
JuanPabloNSZ <juanpablo.nsz@gmail.com>
2022-09-05 20:11:38 -0400

    Create models/Message.js

---
+++ models/Message.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const { required } = require('nodemon/lib/config');
+
+const MessageSchema = new mongoose.Schema({
+	userId: {
+		type: String,
+		required: true,
+	},
+	message: {
+		type: String,
+		required: true,
+	},
+	likes: {
+		type: Number,
+		required: true,
+	},
+	replies: {
+		type: Array,
+	},
+});
+
+module.exports = mongoose.model('Message', MessageSchema);
