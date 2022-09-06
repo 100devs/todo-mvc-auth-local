d9274f0c51de6a58ad51e3ffbe5ebe61dccf8341
Steven Gilliland <s.gonza.gills@gmail.com>
2022-09-05 22:17:10 -0400

    update listPost terminology

--- controllers/blogPost.js
+++ controllers/blogPost.js
@@ -5,8 +5,8 @@ module.exports = {
         console.log(req.user)
         try{
             const postItems = await BlogPost.find({userId:req.user.id}).sort({date: -1})
-            const itemsLeft = await BlogPost.countDocuments({userId:req.user.id})
-            res.render('listPosts.ejs', {BlogPost: postItems, left: itemsLeft, user: req.user})
+            const numberOfPosts = await BlogPost.countDocuments({userId:req.user.id})
+            res.render('listPosts.ejs', {BlogPost: postItems, postCount: numberOfPosts, user: req.user})
         }catch(err){
             console.log(err)
         }
