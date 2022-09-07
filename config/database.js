aa2f59a29b114b4999628a5bd90f5d17d4ab8ce7
MustafaAliDS <dsalimustafa@gmail.com>
2022-09-06 17:22:50 -0700

    chore: changed database.js lines to remove deprecated connection options

--- config/database.js
+++ config/database.js
@@ -2,7 +2,10 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.DB_STRING, {});
+    const conn = await mongoose.connect(process.env.DB_STRING, {
+      useFindAndModify: false,
+      useCreateIndex: true,
+    });
 
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (err) {
