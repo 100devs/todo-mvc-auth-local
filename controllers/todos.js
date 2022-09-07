517e206b38af2fa88f583baaed5345c5d71d56ed
Justin Moy <moy.justin94@gmail.com>
2022-09-07 15:21:46 -0500

    updates

--- controllers/todos.js
+++ controllers/todos.js
@@ -4,7 +4,7 @@ module.exports = {
     getTodos: async (req,res)=>{
         console.log(req.user)
         try{
-            const todoItems = await Todo.find({userId:req.user.id})
+            const todoItems = await Todo.find({userId:req.user.id}) //find the todo in database with matching from models
             const itemsLeft = await Todo.countDocuments({userId:req.user.id,completed: false})
             res.render('todos.ejs', {todos: todoItems, left: itemsLeft, user: req.user})
         }catch(err){
