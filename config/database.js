dcde58850f718cad3853708992a21a5be38b8571
sara <sruthfd@csu.fullerton.edu>
2023-05-25 13:47:02 -0700

    Add sarafurd

--- config/database.js
+++ config/database.js
@@ -1,19 +1,20 @@
 const mongoose = require('mongoose')
 
-const connectDB = async () => {
-  try {
-    const conn = await mongoose.connect(process.env.DB_STRING, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useFindAndModify: false,
-      useCreateIndex: true
-    })
-
-    console.log(`MongoDB Connected: ${conn.connection.host}`)
-  } catch (err) {
-    console.error(err)
-    process.exit(1)
-  }
+//connectDB function
+const connectDB = async() => {
+    try {
+        const conn = await mongoose.connect(process.env.DB_STRING, {
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+                useFindAndModify: false,
+                useCreateIndex: true
+            })
+            //Telling the user that they're connected to the MongoDB
+        console.log(`MongoDB Connected: ${conn.connection.host}`)
+    } catch (err) {
+        console.error(err)
+        process.exit(1)
+    }
 }
 
 module.exports = connectDB
