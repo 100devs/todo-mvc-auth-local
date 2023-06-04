8ca8e023cdfd8aedbb6387fd55962a3cb0b05dfb
Manuel Barreiro <manuel.barreiro@ing.austral.edu.ar>
2023-06-04 20:43:07 -0300

    fixed logout

--- controllers/auth.js
+++ controllers/auth.js
@@ -37,15 +37,17 @@ const User = require('../models/User')
   }
   
   exports.logout = (req, res) => {
-    req.logout(() => {
-      console.log('User has logged out.')
-    })
-    req.session.destroy((err) => {
-      if (err) console.log('Error : Failed to destroy the session during logout.', err)
-      req.user = null
-      res.redirect('/')
-    })
-  }
+    req.logout(function (err) {
+      if (err) {
+        return next(err);
+      }
+      req.session.destroy((err) => {
+        if (err) console.log("Error : Failed to destroy the session during logout.", err);
+        req.user = null;
+        res.redirect("/");
+      });
+    });
+  };
   
   
   exports.getSignup = (req, res) => {
