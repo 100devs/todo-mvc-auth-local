3956abe8e938d82240cb0547a973e3a18e7a3c7d
Manuel Barreiro <manuel.barreiro@ing.austral.edu.ar>
2023-06-04 19:18:46 -0300

    adapted Mongoose and connect-mongo to current v.

--- controllers/auth.js
+++ controllers/auth.js
@@ -47,6 +47,7 @@ const User = require('../models/User')
     })
   }
   
+  
   exports.getSignup = (req, res) => {
     if (req.user) {
       return res.redirect('/todos')
@@ -56,41 +57,54 @@ const User = require('../models/User')
     })
   }
   
-  exports.postSignup = (req, res, next) => {
-    const validationErrors = []
-    if (!validator.isEmail(req.body.email)) validationErrors.push({ msg: 'Please enter a valid email address.' })
-    if (!validator.isLength(req.body.password, { min: 8 })) validationErrors.push({ msg: 'Password must be at least 8 characters long' })
-    if (req.body.password !== req.body.confirmPassword) validationErrors.push({ msg: 'Passwords do not match' })
+  exports.postSignup = async (req, res, next) => {
+    try {
+      const validationErrors = [];
+      if (!validator.isEmail(req.body.email))
+        validationErrors.push({ msg: 'Please enter a valid email address.' });
+      if (!validator.isLength(req.body.password, { min: 8 }))
+        validationErrors.push({ msg: 'Password must be at least 8 characters long' });
+      if (req.body.password !== req.body.confirmPassword)
+        validationErrors.push({ msg: 'Passwords do not match' });
   
-    if (validationErrors.length) {
-      req.flash('errors', validationErrors)
-      return res.redirect('../signup')
-    }
-    req.body.email = validator.normalizeEmail(req.body.email, { gmail_remove_dots: false })
-  
-    const user = new User({
-      userName: req.body.userName,
-      email: req.body.email,
-      password: req.body.password
-    })
-  
-    User.findOne({$or: [
-      {email: req.body.email},
-      {userName: req.body.userName}
-    ]}, (err, existingUser) => {
-      if (err) { return next(err) }
-      if (existingUser) {
-        req.flash('errors', { msg: 'Account with that email address or username already exists.' })
-        return res.redirect('../signup')
+      if (validationErrors.length) {
+        req.flash('errors', validationErrors);
+        return res.redirect('../signup');
       }
-      user.save((err) => {
-        if (err) { return next(err) }
-        req.logIn(user, (err) => {
-          if (err) {
-            return next(err)
-          }
-          res.redirect('/todos')
-        })
-      })
-    })
-  }
+      req.body.email = validator.normalizeEmail(req.body.email, {
+        gmail_remove_dots: false,
+      });
+  
+      const existingUser = await User.findOne({
+        $or: [
+          { email: req.body.email },
+          { userName: req.body.userName },
+        ],
+      }).exec();
+  
+      if (existingUser) {
+        req.flash('errors', {
+          msg: 'Account with that email address or username already exists.',
+        });
+        return res.redirect('../signup');
+      }
+  
+      const user = new User({
+        userName: req.body.userName,
+        email: req.body.email,
+        password: req.body.password,
+      });
+  
+      await user.save();
+  
+      req.logIn(user, (err) => {
+        if (err) {
+          return next(err);
+        }
+        res.redirect('/todos');
+      });
+    } catch (err) {
+      return next(err);
+    }
+  };
+  
