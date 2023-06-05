8b54735ef6a8287c1e7002b9d3bc8dc8de03aed7
Manuel Barreiro <manuel.barreiro@ing.austral.edu.ar>
2023-06-04 21:49:29 -0300

    updated version

--- config/database.js
+++ config/database.js
@@ -5,8 +5,6 @@ const connectDB = async () => {
     const conn = await mongoose.connect(process.env.DB_STRING, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      useFindAndModify: false,
-      useCreateIndex: true
     })
 
     console.log(`MongoDB Connected: ${conn.connection.host}`)
--- config/passport.js
+++ config/passport.js
@@ -3,31 +3,43 @@ const mongoose = require('mongoose')
 const User = require('../models/User')
 
 module.exports = function (passport) {
-  passport.use(new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
-    User.findOne({ email: email.toLowerCase() }, (err, user) => {
-      if (err) { return done(err) }
-      if (!user) {
-        return done(null, false, { msg: `Email ${email} not found.` })
-      }
-      if (!user.password) {
-        return done(null, false, { msg: 'Your account was registered using a sign-in provider. To enable password login, sign in using a provider, and then set a password under your user profile.' })
-      }
-      user.comparePassword(password, (err, isMatch) => {
-        if (err) { return done(err) }
-        if (isMatch) {
-          return done(null, user)
+  passport.use(
+    new LocalStrategy({ usernameField: 'email' }, async (email, password, done) => {
+      try {
+        const user = await User.findOne({ email: email.toLowerCase() }).exec();
+
+        if (!user) {
+          return done(null, false, { msg: `Email ${email} not found.` });
         }
-        return done(null, false, { msg: 'Invalid email or password.' })
-      })
+        if (!user.password) {
+          return done(null, false, {
+            msg: 'Your account was registered using a sign-in provider. To enable password login, sign in using a provider, and then set a password under your user profile.',
+          });
+        }
+
+        user.comparePassword(password, (err, isMatch) => {
+          if (err) { return done(err) }
+          if (isMatch) {
+            return done(null, user)
+          }
+          return done(null, false, { msg: 'Invalid email or password.' })
+        })
+      } catch (err) {
+        return done(err);
+      }
     })
-  }))
-  
+  )
 
   passport.serializeUser((user, done) => {
     done(null, user.id)
   })
 
-  passport.deserializeUser((id, done) => {
-    User.findById(id, (err, user) => done(err, user))
-  })
+  passport.deserializeUser(async (id, done) => {
+    try {
+      const user = await User.findById(id).exec();
+      done(null, user);
+    } catch (err) {
+      done(err, null);
+    }
+  });
 }
