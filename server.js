53fe6792c34a456c04e2759bc5172b19c06905ee
Espar-RodBar <cr4wl3r@gmail.com>
2023-10-08 19:01:30 +0200

    Added active port on console.log in callback app.listen

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use(
     secret: 'keyboard cat',
     resave: false,
     saveUninitialized: false,
-    store: new MongoStore({ mongooseConnection: mongoose.connection })
+    store: new MongoStore({ mongooseConnection: mongoose.connection }),
   })
 )
 
@@ -42,5 +42,5 @@ app.use('/', mainRoutes)
 app.use('/todos', todoRoutes)
 
 app.listen(process.env.PORT, () => {
-  console.log('Server is running, you better catch it!')
+  console.log(`Server running on port: ${process.env.PORT}`)
 })
